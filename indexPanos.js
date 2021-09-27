6c570c7224e788f70656711b66c2dc98062b2bf8
annarylander <91195313+annarylander@users.noreply.github.com>
2021-09-27 15:17:04 +0200

    Update indexPanos.js

--- indexPanos.js
+++ indexPanos.js
@@ -1,84 +1,104 @@
 const coffees = [
-    {name: 'Brygg Kaffe', price: 20},
-    {name: 'Cappucino', price: 30},
-    {name: 'Latte', price: 40}
-  ]
-  
-  
-  class Customer{
-      constructor(){
-          this.allTransactions = []
-          this.membership = "Brons"
-          this.numberOfCups = 0
-          this.transactionSum = 0
-      }
-  
-      getTransaction(){
-        //   printTransaction = this.allTransactions.forEach(element => {
-        //     console.log(`Du köpte ${this.allTransactions.numberOfCups} st ${this.allTransactions.typeOfCoffee} för ${coffees[0].price} kr styck. Summa: ${this.allTransactions.sumOfTransaction}`)
-            
-            // return `Du köpte ${this.allTransactions.numberOfCups} st ${this.allTransactions.typeOfCoffee} för ${coffees[0].price} kr styck. Summa: ${this.allTransactions.sumOfTransaction}`
+    { name: 'Bryggkaffe', price: 20 },
+    { name: 'Cappucino', price: 30 },
+    { name: 'Latte', price: 40 }
+]
 
-            // return `Du köpte ${this.allTransactions.numberOfCups} st ${this.allTransactions.typeOfCoffee} för ${coffees[0].price} kr styck. Summa: ${this.allTransactions.sumOfTransaction}`
-        //   })
-           
-        //   return this.allTransactions
-      }
-  
-     addTransaction(typeOfCoffee, numberOfCups){
-          let emptyArray = []
-          let sumOfTransaction = 0
-          if(typeOfCoffee === "Bryggkaffe") {
-              sumOfTransaction = numberOfCups * 20
-              emptyArray = ["Bryggkaffe", numberOfCups, sumOfTransaction]
-              console.log(`Du köpte ${numberOfCups} st ${typeOfCoffee} för ${coffees[0].price} kr styck. Summa: ${sumOfTransaction}`)
-              this.allTransactions.push(emptyArray)
-          }else if (typeOfCoffee === "Cappucino") {
-              sumOfTransaction = numberOfCups * 30
-              emptyArray = ["Cappucino", numberOfCups, sumOfTransaction]
-              console.log(`Du köpte ${numberOfCups} st ${typeOfCoffee} för ${coffees[1].price} kr styck. Summa: ${sumOfTransaction}`)
-              this.allTransactions.push(emptyArray)
-          }else if(typeOfCoffee === "Latte") {
-              sumOfTransaction = numberOfCups * 40
-              emptyArray = ["Latte", numberOfCups, sumOfTransaction]
-              console.log(`Du köpte ${numberOfCups} st ${typeOfCoffee} för ${coffees[2].price} kr styck. Summa: ${sumOfTransaction}`)
-              this.allTransactions.push(emptyArray)
-          }
-      }
-  
-      getTotalSpend(){
-          let sumOfTotal = 0;
-          
-          for(let i = 0; i < this.allTransactions.length; i++){
-              sumOfTotal += this.allTransactions[i][2]
-          }
-          return sumOfTotal;
-      }
-  
-      // getMembershipStatus(){
-      //     return this.membership
-      // }
-  
-      // checkMembershipStatus(){
-      //     return this.membership
-      // }
-  
-  }
-  
-  
-  const customer1 = new Customer()
-  
-  customer1.addTransaction("Bryggkaffe", 2)
-  customer1.addTransaction("Cappucino", 1)
-  customer1.addTransaction("Latte", 1)
-  
-//   customer1.getTransaction()
-  
-  //Det skriver ut: [ [ 'Bryggkaffe', 3, 60 ], [ 'Bryggkaffe', 2, 40 ] ]
-//   console.log(customer1.allTransactions)
+class Customer {
+    constructor() {
+        this.transactions = []
+        this.membership = "Brons"
+        this.numberOfCups = 0
+        this.transactionSum = 0
 
-//   customer1.getTransaction()
-  
-  //Nu funkar getTotalSpend()
-  console.log(customer1.getTotalSpend())
-  
+    }
+
+    getMembershipStatus() {
+        return this.membership
+    }
+
+    updateMembershipStatus() {
+        if (this.numberOfCups >= 10 && this.numberOfCups < 30) {
+            this.membership = "Silver"
+        }
+        else if (this.numberOfCups >= 30) {
+            this.membership = "Guld"
+        }
+        return this.membership
+    }
+
+
+    getTransaction() {
+        return this.transactions
+    }
+
+    getTotalSpent() {
+        let sum = 0
+        for (let i = 0; i < this.transactions.length; i++) {
+            const element = this.transactions[i]
+            sum += (element[0].price * element[0].numberOfCups)
+        }
+        this.transactionSum = sum
+
+    }
+
+ 
+
+
+
+    addTransaction(typeOfCoffee, numberOfCups) {
+        let emptyArray = []
+        if (typeOfCoffee === "Bryggkaffe") {
+            emptyArray = [{ type: "Bryggkaffe", numberOfCups: numberOfCups, price: coffees[0].price }]
+            this.transactions.push(emptyArray)
+
+        } else if (typeOfCoffee === "Cappuccino") {
+            emptyArray = [{ type: "Cappuccino", numberOfCups: numberOfCups, price: coffees[1].price }]
+            this.transactions.push(emptyArray)
+
+        } else if (typeOfCoffee === "Latte") {
+            emptyArray = [{ type: "Latte", numberOfCups: numberOfCups, price: coffees[2].price }]
+            this.transactions.push(emptyArray)
+
+        }
+
+
+    }
+
+    getTotalCups() {
+        let sum = 0
+        for (let i = 0; i < this.transactions.length; i++) {
+            const element = this.transactions[i]
+            sum += element[0].numberOfCups
+        }
+        this.numberOfCups = sum
+
+    }
+
+    writeMessage() {
+        for (let i = 0; i < this.transactions.length; i++) {
+            const element = this.transactions[i]
+            if (element.typeOfCoffee === "Bryggkaffe") {
+            console.log(`Du har köpt ${element[0].numberOfCups} st ${element[0].typeOfCoffee} för ${coffees[0].price} kr/st Summa:${this.transactionSum}`)
+        }
+        
+    }
+    }
+}
+
+let customer1 = new Customer()
+customer1.addTransaction("Bryggkaffe", 5)
+customer1.addTransaction("Cappuccino", 50)
+customer1.addTransaction("Latte", 4)
+customer1.getTotalCups()
+customer1.getTotalSpent()
+console.log(customer1.numberOfCups)
+console.log(customer1.transactionSum)
+console.log(customer1.updateMembershipStatus())
+customer1.writeMessage()
+
+
+
+//console.log(customer1.getTotalSpent())
+//console.log(customer1.transactions)
+//console.log(customer1.getTotalCups())
